4237219b91020209aebe1f2efe8f1426ff019f8a
Rameshkumarsahutmr <115208196+Rameshkumarsahutmr@users.noreply.github.com>
2024-08-06 23:35:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 import express from "express";
 import { getvmovie, getvserie } from "./src/vidsrcto.js";
 
-
 const port = 3000;
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
     res.status(200).json({
         intro: "Welcome to the unofficial vidsrc provider: check the provider website @ https://vidsrc.cc/ ",
         routes: {
-            movie: "/vidsrc/:movieTMDBid",
-            show: "/vidsrc/:showTMDBid?s=seasonNumber&e=episodeNumber"
+            movie: "/embed/:tmdbId",
+            show: "/embed/:tmdbId?s=seasonNumber&e=episodeNumber"
         },
         author: "This api is developed and created by Inside4ndroid Studios"
     });
 });
 
-app.get('/vidsrc/:tmdbId', async (req, res) => {
+app.get('/embed/:tmdbId', async (req, res) => {
     const id = req.params.tmdbId;
     const season = req.query.s;
     const episode = req.query.e;
